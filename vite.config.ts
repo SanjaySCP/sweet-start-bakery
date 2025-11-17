75dae2f7794de5470599d0bf449b06dcf73c743b
Sanjay S <133010957+SanjaySCP@users.noreply.github.com>
2025-11-17 19:52:24 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,8 @@ import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode base: "/sweet-start-bakery/", }) => ({
+export default defineConfig(({ mode }) => ({
+  base: "/sweet-start-bakery/",
   server: {
     host: "::",
     port: 8080,
